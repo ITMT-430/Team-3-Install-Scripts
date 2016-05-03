8d60ae30a148dd9dde443b15ad0f630b6f50dc21
thegeekkid <brian@geekkidconsulting.com>
2016-05-03 11:34:56 -0500

    Try to export term var

--- Production/prod-deploy.sh
+++ Production/prod-deploy.sh
@@ -63,6 +63,7 @@ DBPASSWD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
 
 apt-get install -y debconf-utils
+export TERM=xterm
 
 clear
   echo '****************************************'
